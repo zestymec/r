11b4e2abaf60629281f1fc962b97db394d53b495
zestymec <m.umer394aziz@gmail.com>
2026-04-09 10:38:15 +0500

    code updated

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text , Style } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style{{ 'Flex:1'}}>App</Text>
+    </View>
+  )
+}
+
+export default App
