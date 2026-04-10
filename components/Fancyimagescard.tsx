70c21f284badc9f7c3f1b003d6e9631368234fd2
zestymec <m.umer394aziz@gmail.com>
2026-04-10 12:59:32 +0500

    fancy card created

--- components/Fancyimagescard.tsx
+++ components/Fancyimagescard.tsx
@@ -1,25 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function Fancyimagescard() {
   return (
-    <View>
-      <Text  style={styles.headingtext}>Fancyimagescard</Text>
-      <View style={[styles.card , ]}>
+   
+      <View>
+        <Text style={styles.headingtext}>Fancyimagescard</Text>
+        <View style={[styles.card,]}>
+          <Image
+            source={
+              { uri: 'https://avatars.githubusercontent.com/u/171046526?v=4' }
 
+            }
+            style={styles.cardimage} />
+        </View>
+       
       </View>
-    </View>
   )
 }
 
 const styles = StyleSheet.create({
-    headingtext: {
-        color: 'blue',
-        fontSize: 24,
-        fontWeight: 'bold',
-        padding: 8,
-    },
-    card:{
-        
-    }
+  headingtext: {
+    color: 'blue',
+    fontSize: 24,
+    fontWeight: 'bold',
+    padding: 8,
+  },
+  cardimage: {
+    width: 100,
+    height: 50,
+    margin: 'auto',
+  }
+
+  , card: {
+
+  }
 })
